2dd8cc9826b61f191292e72d8218279f5288cb50
bokor-git <bogdankorolbk@gmail.com>
2020-05-24 19:45:36 +0300

    create main blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Messenger/>
+        </div>
+    );
 }
+
+
+function Messenger() {
+    return <div className="messenger">
+        <div  className="title"> <h1>Messenger</h1></div>
+        <Search/>
+        <Dialogs/>
+        <MassageList/>
+    </div>
+
+}
+
+function Search() {
+    return <div className="search"><input type="textarea" placeholder="Search"/></div>
+}
+
+function Dialogs() {
+    return <div className="dialogs">
+        <DialogItem/>
+        <DialogItem/>
+        <DialogItem/>
+        <DialogItem/>
+        <DialogItem/>
+    </div>
+}
+
+function DialogItem() {
+    return <div className="dialogs-item">
+        <img className="user-photo" src="https://randomuser.me/api/portraits/men/71.jpg" alt=""/>
+        <div className="user-name">User name</div>
+    </div>
+
+}
+
+function MassageList() {
+    return <div className="massage-list">
+        <MassageItem/>
+        <div className="massage-item">Lorem ipsum dolor sit amet</div>
+        <MassageItem/>
+        <div className="massage-item">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam architecto</div>
+        <MassageItem/>
+        <MassageItem/>
+
+    </div>
+}
+
+function MassageItem() {
+    return <div className="massage-item">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam architecto
+        consectetur cupiditate
+        ducimus et eum eveniet, id inventore ipsam mollitia, nobis, non nulla odio officiis quasi quia velit.
+    </div>
+
+}
+
 
 export default App;
