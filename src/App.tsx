590d2185972e54e74fd927ea3f88136e2bbf5d5d
bokor-git <bogdankorolbk@gmail.com>
2020-05-25 21:46:15 +0300

    add dialog search

--- src/App.tsx
+++ src/App.tsx
@@ -11,34 +11,71 @@ function App() {
 
 
 function Messenger() {
+    type DialogsType = { id: number, name: string }
+    let [dialogs, setDialogs] = useState<Array<DialogsType>>([
+        {id: 1, name: "Boris Jonson"},
+        {id: 2, name: "Alex Brother"},
+        {id: 3, name: "Peter Pen"},
+        {id: 4, name: "Mr. Been"},
+        {id: 5, name: "Alex Alexandrov"}
+    ])
+
+    const filterDialog = (chart:string)=>{
+
+       let filteredDialogs =  dialogs.filter(d => d.name.toLowerCase().includes(chart))
+        setDialogs([...filteredDialogs])
+        if (chart.length===0){
+            setDialogs([
+                {id: 1, name: "Boris Jonson"},
+                {id: 2, name: "Alex Brother"},
+                {id: 3, name: "Peter Pen"},
+                {id: 4, name: "Mr. Been"},
+                {id: 5, name: "Alex Alexandrov"}
+            ])
+        }
+
+    }
+
     return <div className="messenger">
         <div className="title"><h1>Messenger</h1></div>
-        <Search/>
-        <Dialogs/>
+        <Search filterDialog = {filterDialog}/>
+        <Dialogs dialogs={dialogs}/>
         <MassageList/>
 
     </div>
 
 }
 
-function Search() {
-    return <div className="search"><input type="textarea" placeholder="Search"/></div>
+type SearchPropsType= {
+    filterDialog: (chart:string)=>void
 }
 
-function Dialogs() {
+
+function Search({filterDialog}:SearchPropsType) {
+
+    return <div className="search"><input type="textarea" placeholder="Search" onChange={(e)=>filterDialog(e.currentTarget.value)}/></div>
+}
+
+type DialogsPropsType = {
+    dialogs:Array<{ id: number, name: string }>
+}
+
+function Dialogs({dialogs}:DialogsPropsType) {
+
     return <div className="dialogs">
-        <DialogItem/>
-        <DialogItem/>
-        <DialogItem/>
-        <DialogItem/>
-        <DialogItem/>
+        {dialogs.map(d => <DialogItem key={d.id} dialog={d}/>)}
+
     </div>
 }
 
-function DialogItem() {
+type DialogItemPropsType = {
+    dialog: { id: number, name: string }
+}
+
+function DialogItem({dialog}: DialogItemPropsType) {
     return <div className="dialogs-item">
         <img className="user-photo" src="https://randomuser.me/api/portraits/men/71.jpg" alt=""/>
-        <div className="user-name">User name</div>
+        <div className="user-name">{dialog.name}</div>
     </div>
 
 }
@@ -55,7 +92,7 @@ function MassageList() {
         {title: "Hello again", date: "12.06.2020"}
     ]
 
-    type MassageType= { title: string, date: string }
+    type MassageType = { title: string, date: string }
     let [massage, setMassage] = useState<Array<MassageType>>(initState)
     let [newMassage, setNewMassage] = useState<string>("")
 
@@ -64,7 +101,6 @@ function MassageList() {
     let mm = String(today.getMonth() + 1).padStart(2, '0');
     let yyyy = today.getFullYear();
     let date = dd + '.' + mm + '.' + yyyy;
-
 
 
     const addNewMassage = () => {
@@ -99,7 +135,7 @@ type MassageItemPropsType = {
 }
 
 function MassageItem({massage}: MassageItemPropsType) {
-    return (<div className="massage-container" >
+    return (<div className="massage-container">
         <div className="massage-date">{massage.date}</div>
         <div className="massage-item">{massage.title}</div>
     </div>)
