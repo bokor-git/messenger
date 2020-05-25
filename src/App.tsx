881368229cef5288ee3c39e20bf3d59111ecbcf6
bokor-git <bogdankorolbk@gmail.com>
2020-05-25 20:39:44 +0300

    add date for massage

--- src/App.tsx
+++ src/App.tsx
@@ -45,27 +45,48 @@ function DialogItem() {
 
 function MassageList() {
 
-    let initState = ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam architecto",
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam architecto",
-        "consectetur cupiditate", "Lorem ipsum dolor sit amet",
-        "Hello again"
+    let initState = [{
+        title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam architecto",
+        date: "12.06.2020"
+    },
+        {title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam architecto", date: "12.06.2020"},
+        {title: "consectetur cupiditate", date: "12.06.2020"},
+        {title: "Lorem ipsum dolor sit amet", date: "12.06.2020"},
+        {title: "Hello again", date: "12.06.2020"}
     ]
-    let [massage, setMassage] = useState<Array<string>>(initState)
+
+    type MassageType= { title: string, date: string }
+    let [massage, setMassage] = useState<Array<MassageType>>(initState)
     let [newMassage, setNewMassage] = useState<string>("")
 
-    const addNewMassage = ()=> {
-     setMassage([...massage, newMassage])
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    let date = dd + '.' + mm + '.' + yyyy;
+
+
+
+    const addNewMassage = () => {
+        let newMessage = {title: newMassage, date: date}
+        const newMassages = [...massage, newMessage]
+
+        setMassage(newMassages)
         setNewMassage("")
     }
 
-    // @ts-ignore
-    const onEnterPress = (e:KeyboardEvent<HTMLInputElement>) => {if (e.charCode === 13) {addNewMassage()}}
+    const onEnterPress = (e: any) => {
+        if (e.charCode === 13) {
+            addNewMassage()
+        }
+    }
     return <div className="massage-list">
         {massage.map((m) => <MassageItem massage={m}/>)}
 
 
         <div className="new-massage">
-            <input onChange={e => setNewMassage(e.currentTarget.value)} onKeyPress={onEnterPress} type="text" value={newMassage}
+            <input onChange={e => setNewMassage(e.currentTarget.value)} onKeyPress={onEnterPress} type="text"
+                   value={newMassage}
                    placeholder="Enter your massage"/>
 
         </div>
@@ -74,11 +95,15 @@ function MassageList() {
 }
 
 type MassageItemPropsType = {
-    massage: string
+    massage: { title: string, date: string }
 }
 
 function MassageItem({massage}: MassageItemPropsType) {
-    return <div className="massage-item">{massage} </div>
+    return (<div className="massage-container" >
+        <div className="massage-date">{massage.date}</div>
+        <div className="massage-item">{massage.title}</div>
+    </div>)
+
 
 }
 
